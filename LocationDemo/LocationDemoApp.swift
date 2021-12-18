3037ad8791695fac06d28851a90e30f7f091643d
Aybars Acar <aybars.dev@gmail.com>
2021-12-19 10:01:07 +1100

    Initial Commit

---
+++ LocationDemo/LocationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationDemoApp.swift
+//  LocationDemo
+//
+//  Created by Aybars Acar on 19/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct LocationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
